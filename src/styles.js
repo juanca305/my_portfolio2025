9181d7dae47fcd79cf15e883063ed514e0bd66ad
Juan Carlos Rodriguez <jc.rdguez64@gmail.com>
2025-08-11 10:44:57 -0400

    Adjusted text-size in  heroHeadText

--- src/styles.js
+++ src/styles.js
@@ -4,7 +4,7 @@ const styles = {
   padding: "sm:px-16 px-6 sm:py-16 py-10",
 
   heroHeadText:
-    "font-black text-white lg:text-[64px] sm:text-[48px] xs:text-[50px] text-[40px] lg:leading-[80px] mt-6",
+    "font-black text-white lg:text-[64px] sm:text-[48px] xs:text-[50px] text-[32px] lg:leading-[80px] mt-6",
   heroSubText:
     "text-[#dfd9ff] font-medium lg:text-[24px] sm:text-[22px] xs:text-[20px] text-[16px] lg:leading-[40px]",
 
